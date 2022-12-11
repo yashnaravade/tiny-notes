e296410afdba196707239d9c94dca635e970f7a6
Yash Naravade <yashdip123@gmail.com>
2022-12-11 19:12:22 +0530

    Components and router setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import './App.css'
+import AddNote from './views/AddNote/AddNote'
+import Home from './views/Home/Home'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
   <>
+<BrowserRouter>
+
+<Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/add-note" element={<AddNote />} />
+</Routes>
+</BrowserRouter>
   </>
   )
 }
---
+++ src/views/AddNote/AddNote.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./AddNote.css";
+
+function AddNote() {
+  return <div>
+    <h3>AddNote</h3>
+  </div>;
+}
+
+export default AddNote;
